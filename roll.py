5b7aa3a9f2adae160847f3a7c80d5a80553c6363
AlvaroRuizDelgado <alvaro.ruiz.delgado@gmail.com>
2019-05-08 08:51:06 +0900

    Added roll script and unfinished unit tests

---
+++ roll.py
@@ -0,0 +1,139 @@
+#!/usr/local/bin/python3
+# Last edited: 19/05/07
+
+import sys
+import math
+from random import randint
+
+# Constants
+FAIL_THRESHOLD = 3
+EXPLODE_THRESHOLD = 90
+INHUMAN_THRESHOLD = 320
+ZEN_THRESHOLD = 440
+
+def roll(argv):
+    if (len(argv) == 0 or "--help" in argv or "-h" in argv):
+        print_help()
+        sys.exit(0)
+
+    # Roll characteristics
+    die_range = 100
+    dice_number = 1
+    modifier = 0
+    difficulty = 0
+    fail_modifier = 0
+
+    # Get the arguments
+    roll_type = argv.pop(0)
+    if (roll_type == "d100"):
+        die_range = 100
+    elif (roll_type == "d10"):
+        die_range = 10
+    else:
+        print("Error: The accepted values are 'd100' and 'd10'")
+        print_help()
+        sys.exit(1)
+
+    while len(argv) > 0:
+        if (argv[0] == "--difficulty" or argv[0] == "-d"):
+            argv.pop(0)
+            difficulty = int(argv.pop(0))
+        elif (argv[0] == "--master" or argv[0] == "-m"):
+            argv.pop(0)
+            fail_modifier = -1
+        else:
+            modifier = int(argv.pop(0))
+    print("Roll type:", roll_type, "/ Modifier:", modifier, "/ Difficulty:", difficulty, "/ Fail modifier:", fail_modifier)
+
+    # Show results
+    class bcolors:
+        PURPLE = '\033[95m'
+        GREY = '\033[92m'
+        ORANGE = '\033[91m'
+        LIGHT_RED = '\033[35m'
+        YELLOW = '\033[0;32m'
+        BLUE = '\033[0;34m'
+        TEST = '\033[0;36m'
+
+    # Find the result and print it out
+    if (roll_type == "d100"):
+        single_roll = randint(1,die_range)
+        die_roll = single_roll
+        explode_value = EXPLODE_THRESHOLD
+        while (single_roll > explode_value):
+            single_roll = randint(1,die_range)
+            die_roll = die_roll + single_roll
+            explode_value = explode_value + 1
+        
+        roll_result = "not assigned"
+        if (die_roll <= FAIL_THRESHOLD+fail_modifier):
+            font_roll_color = bcolors.GREY
+            font_result_color = bcolors.PURPLE
+            fail_level = randint(1,100)
+            roll_result = "CRITICAL FAIL!! ("+str(fail_level)+")"
+        else:
+            die_roll = die_roll + modifier
+            if (difficulty != 0 and die_roll < difficulty):
+                font_roll_color = bcolors.GREY
+                font_result_color = bcolors.BLUE
+                roll_result  = "FAIL!"
+            else:
+                font_roll_color = bcolors.YELLOW
+                font_result_color = bcolors.ORANGE
+                if (die_roll >= ZEN_THRESHOLD):
+                    roll_result = "ZEN!!"
+                elif (die_roll >= INHUMAN_THRESHOLD):
+                    roll_result = "INHUMAN!"
+                else:
+                    roll_result = "SUCCESS"
+        print(font_roll_color, die_roll, bcolors.LIGHT_RED, "-->", font_result_color, roll_result, bcolors.GREY)
+        return(
+                  {
+                    'die_range': die_range,
+                    'die_roll': die_roll,
+                    'roll_result': roll_result
+                  }
+              )
+
+    elif (roll_type == "d10"):
+        die_roll = randint(1,die_range)
+        if (die_roll == 10):
+            die_roll = 12
+        die_roll = die_roll + modifier
+        roll_result = "not assigned"
+        if (difficulty == 0):
+            difficulty = 10
+        if (die_roll < difficulty):
+            font_roll_color = bcolors.GREY
+            font_result_color = bcolors.BLUE
+            roll_result  = "FAIL!"
+        else:
+            font_roll_color = bcolors.YELLOW
+            font_result_color = bcolors.ORANGE
+            success_level = die_roll - difficulty
+            roll_result = "SUCCESS (level "+str(success_level)+")"
+        print(font_roll_color, die_roll, bcolors.LIGHT_RED, "-->", font_result_color, roll_result, bcolors.GREY)
+        return(
+                  {
+                    'die_range': die_range,
+                    'die_roll': die_roll,
+                    'roll_result': roll_result
+                  }
+              )
+
+def print_help():
+    print("Usage: pass the type of die to roll, followed by the modifier in the d100 case and other options")
+    print("  - d100 +modifier")
+    print("  - d10")
+    print("Options:")
+    print("  -d, --difficulty, set the difficulty.")
+    print("  -m, --master, failure % is reduced by 1 (i.e. '3' is not a failure)")
+    print("Examples:")
+    print("  ./roll d100 +30 -d 120")
+    print("  ./roll d100 +15 -d 80 -m")
+    print("  ./roll d100 -20")
+    print("  ./roll d100")
+    print("  ./roll d10")
+
+if __name__ == "__main__":
+   roll(sys.argv[1:])
---
+++ test_roll.py
@@ -0,0 +1,85 @@
+from roll import roll
+import unittest
+from unittest import mock   # pylint: disable=E0611
+
+class TestRoll(unittest.TestCase):
+
+    examples_d100 = [ 
+        #{'die_range': 100, 'difficulty': 10, 'die_roll': 10, 'victory_points': 6, 'victory_dice': 6},
+        #{'die_range': 100, 'difficulty': 10, 'die_roll': 11, 'victory_points': 0, 'victory_dice': 0},
+        #{'die_range': 100, 'difficulty': 10, 'die_roll': 20, 'victory_points': 0, 'victory_dice': 0},
+        #{'die_range': 100, 'difficulty': 15, 'die_roll': 14, 'victory_points': 4, 'victory_dice': 4},
+        #{'die_range': 100, 'difficulty': 8, 'die_roll': 12, 'victory_points': 0, 'victory_dice': 0},
+        #{'die_range': 100, 'difficulty': 27, 'die_roll': 15, 'victory_points': 5, 'victory_dice': 8},
+        #{'die_range': 100, 'difficulty': 20, 'die_roll': 19, 'victory_points': 0, 'victory_dice': 0},
+        {'die_range': 100, 'modifier': 30, 'difficulty': 40, 'fail_modifier': 0, 'die_roll': 60, 'roll_result': 'SUCCESS'}
+    ]
+
+    examples_d10 = [ 
+        #{'die_range': 10, 'dice_number': 6, 'difficulty': 4, 'die_roll': 2, 'success_rolls': 6},
+        #{'die_range': 10, 'dice_number': 4, 'difficulty': 4, 'die_roll': 3, 'success_rolls': 4},
+        #{'die_range': 10, 'dice_number': 5, 'difficulty': 4, 'die_roll': 4, 'success_rolls': 5},
+        #{'die_range': 10, 'dice_number': 8, 'difficulty': 4, 'die_roll': 5, 'success_rolls': 0},
+        #{'die_range': 10, 'dice_number': 2, 'difficulty': 4, 'die_roll': 6, 'success_rolls': 0},
+        #{'die_range': 10, 'dice_number': 8, 'difficulty': 2, 'die_roll': 3, 'success_rolls': 0},
+        #{'die_range': 10, 'dice_number': 2, 'difficulty': 6, 'die_roll': 6, 'success_rolls': 2}
+        {'die_range': 10}
+    ]
+
+    def setUp(self):
+        pass
+
+    def test_help(self):
+        with self.assertRaises(SystemExit) as cm:
+            roll([])
+        self.assertEqual(0, cm.exception.code)
+        with self.assertRaises(SystemExit) as cm:
+            roll(['-h'])
+        self.assertEqual(0, cm.exception.code)
+        with self.assertRaises(SystemExit) as cm:
+            roll(['--help'])
+        self.assertEqual(0, cm.exception.code)
+
+    def test_wrong_input(self):
+        with self.assertRaises(SystemExit) as cm:
+            roll(['d15', 10])
+        self.assertEqual(1, cm.exception.code)
+
+    @mock.patch('roll.randint')
+    def test_d100_working_mock(self, mock_randint):
+        mock_randint.return_value = 10
+        roll(['d100'])
+        self.assertTrue(mock_randint.called)
+
+    @mock.patch('roll.randint')
+    def test_d100_test(self, mock_randint):
+        for expectation in self.examples_d100:
+            mock_randint.return_value = expectation['die_roll']
+            actual = roll(['d100', expectation['modifier'], '-d', expectation['difficulty']])
+            test_result(self, expectation, actual)
+
+#    @mock.patch('roll.randint')
+#    def test_d10_working_mock(self, mock_randint):
+#        mock_randint.return_value = 10
+#        roll(['d10', 7])
+#        self.assertTrue(mock_randint.called)
+
+#    @mock.patch('roll.randint')
+#    def test_d10_test(self, mock_randint):
+#        for expectation in self.examples_d6:
+#            mock_randint.return_value = expectation['die_roll']
+#            actual = roll(['d10', expectation['dice_number'], '-d', expectation['difficulty']])
+#            test_result(self, expectation, actual)
+
+def test_result(self, expectation, actual):
+    self.assertEqual( expectation['die_range'],   actual['die_range'] )
+    self.assertEqual( expectation['die_roll']+expectation['modifier'],    actual['die_roll'] )
+    self.assertEqual( expectation['roll_result'],  actual['roll_result'] )
+#    if actual['die_range'] == 100:
+#        self.assertEqual( expectation['victory_points'], actual['victory_points'] )
+#        self.assertEqual( expectation['victory_dice'],   actual['victory_dice'] )
+#    elif actual['die_range'] == 10:
+#        self.assertEqual( expectation['success_rolls'], actual['success_rolls'] )
+
+if __name__ == '__main__':
+    unittest.main()
